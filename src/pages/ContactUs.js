3b811817ab2600d6c251491df04d4aaabba6df71
Pedro Simao <pedro.simao@creditsafe.com>
2021-04-25 12:04:05 +0100

    test form

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ContactUs() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+
+  const handleSubmit = (e) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state }),
+    })
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
   return (
     <div className="formContainer container py-5">
       <div className="row text-center">
@@ -14,16 +38,37 @@ export default function ContactUs() {
           </p>
         </div>
       </div>
-      <form name="contact" netlify>
-        <input type="hidden" name="form-name" value="contact" />
+      <form onSubmit={handleSubmit}>
         <p>
           <label>
-            Name <input type="text" name="name" />
+            Your Name:{" "}
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </label>
         </p>
         <p>
           <label>
-            Email <input type="email" name="email" />
+            Your Email:{" "}
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:{" "}
+            <textarea
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
           </label>
         </p>
         <p>
