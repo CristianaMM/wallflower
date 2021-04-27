c5935baacfd0c35350a7b78ecffc5254661f32e7
Cristiana Monteiro <cristianamelomonteiro@gmail.com>
2021-04-27 17:11:05 +0100

    fixes

--- src/pages/walls/WallItem.js
+++ src/pages/walls/WallItem.js
@@ -8,51 +8,49 @@ export default function WallItem(props) {
 
   const wallDetails = wallsInfo.find((w) => w.name === wallName);
 
-  const wallDisplay = (
-    <>
-      <h2 className="text-center">{wallDetails.name}</h2>
-      <div className="row align-items-center text-center my-4">
-        <div className="col-12 col-md-6">
-          <Carousel
-            className="wallsCarousel"
-            showThumbs
-            infiniteLoop
-            showArrows
-            renderIndicator={() => null}
-            renderThumbs={() =>
-              wallDetails.photos.map((p) => (
-                <img className="thumb" src={p} alt="" />
-              ))
-            }
-          >
-            {wallDetails.photos.map((p, i) => {
-              return (
-                <div
-                  className="itemCarouselImg"
-                  style={{ backgroundImage: `url(${p})` }}
-                ></div>
-              );
-            })}
-          </Carousel>
-        </div>
-        <div className="col-12 col-md-5 offset-md-1 text-justify">
-          <p>{wallDetails.text}</p>
-          <p>
-            <strong>Dimentions:</strong> {wallDetails.dimensions}
-          </p>
-        </div>
-      </div>
-    </>
-  );
-
   return (
     <div className="container wallItem py-5">
-      <button className="btn">
-        <Link to="/walls">
-          <i className="bi bi-backspace"> Back</i>
-        </Link>
-      </button>
-      {wallDetails && wallDisplay}
+      {wallDetails && (
+        <>
+          <h2 className="text-center">{wallDetails.name}</h2>
+          <div className="row align-items-center text-center my-4">
+            <div className="col-12 col-md-6">
+              <Carousel
+                className="wallsCarousel"
+                showThumbs
+                infiniteLoop
+                showArrows
+                renderIndicator={() => null}
+                renderThumbs={() =>
+                  wallDetails.photos.map((p, i) => (
+                    <img
+                      className="thumb"
+                      src={p}
+                      alt=""
+                      key={`image-${wallDetails.name}-${i}`}
+                    />
+                  ))
+                }
+              >
+                {wallDetails.photos.map((p, i) => {
+                  return (
+                    <div
+                      className="itemCarouselImg"
+                      style={{ backgroundImage: `url(${p})` }}
+                    ></div>
+                  );
+                })}
+              </Carousel>
+            </div>
+            <div className="col-12 col-md-5 offset-md-1 text-justify">
+              <p>{wallDetails.text}</p>
+              <p>
+                <strong>Dimentions:</strong> {wallDetails.dimensions}
+              </p>
+            </div>
+          </div>
+        </>
+      )}
       <div className="row text-center">
         <h4 style={{ color: `rgb(59, 80, 63)` }}>HOW TO RENT OUR WALLS:</h4>
         <p>
