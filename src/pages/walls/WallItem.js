3dd64d1b222281f70beaa59c2f1526095da1545e
Cristiana Monteiro <cristianamelomonteiro@gmail.com>
2021-04-27 14:46:58 +0100

    Finished walls section

--- src/pages/walls/WallItem.js
+++ src/pages/walls/WallItem.js
@@ -1,70 +1,69 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
-import elaina from "../../assets/images/elaine.jpg";
-import elaina2 from "../../assets/images/babyWall.jpg";
-import elaina3 from "../../assets/images/event2.jpg";
-import elaina4 from "../../assets/images/whiteandpinkflowers.jpg";
-import peterRabbit from "../../assets/images/peterRabbitWall.jpg";
-import jungle from "../../assets/images/jungleWall.jpg";
-import jungle2 from "../../assets/images/jungleWall2.jpg";
-import frame from "../../assets/images/frame2.jpg";
-import frame2 from "../../assets/images/frame.jpg";
-import frame3 from "../../assets/images/cakeframe.jpg";
+import { wallsInfo } from "../../utils";
 
-export default function WallItem() {
-  const wallsInfo = [
-    {
-      name: "Elaina",
-      photos: [elaina, elaina2, elaina3, elaina4],
-      text: `Our original wall "Elaina" This light ivory wall measures 2.3m x 2.3m,
-    made up with a mix of ivory roses, to create a simple but elegant
-    backdrop. The beauty of this wall, is that we can add colour, light,
-    greenery and trickling ivy to suit any colour scheme or occasion.`,
-    },
-    {
-      name: "Copper Piping Duo",
-      photos: [frame, frame2, frame3],
-      text: `Sometimes less is more!! Our copper piping duo, creates a real centre
-    piece talking point. Our 7ft x 4ft frame comes with an understated
-    garland and matching cake hoop. these look best when against a neutral
-    background. We can add light, colour and extra greenery to suit your
-    style.`,
-    },
-    {
-      name: "Peter Rabbit Party Package",
-      photos: [peterRabbit],
-      text: `Peter Rabbit Party Package Who doesn't love this classic tale? *Peter
-    Rabbit vinyl backdrop, with stand and greenery *Artificial grass, picket
-    fence and rustic box *Plush carrot bunting and all props show in photo.
-    PLUS hire of the Peter Rabbit costume ( Fits adults up to 6ft, shoe size
-    7-11)`,
-    },
-    {
-      name: "Artificial Grass Backdrop",
-      photos: [jungle, jungle2],
-      text: `Our latest addition is the artificial grass backdrop. The 2m x 2m wall
-    is SO versatile. It can be adapted to suit a wide range of events!!We
-    can really dress it up or strip it right back for sports events /
-    parties. Either way it makes an ideal photo opportunity to suit
-    everyone.`,
-    },
-  ];
-  return (
-    <div className="container wallItem text-center  py-5">
-      <h1>Artificial Grass Backdrop</h1>
-      <div className="row">
+export default function WallItem(props) {
+  const { wallName } = props.match.params;
+
+  const wallDetails = wallsInfo.filter((w) => w.name === wallName);
+
+  const wallDisplay = (
+    <>
+      <h2 className="text-center">{wallDetails[0].name}</h2>
+      <div className="row align-items-center text-center my-4">
         <div className="col-12 col-md-6">
-          <Carousel showThumbs={true}>
-            {[jungle, jungle2].map((p, i) => {
+          <Carousel
+            className="wallsCarousel"
+            showThumbs
+            infiniteLoop
+            showArrows
+            renderIndicator={() => null}
+            renderThumbs={() =>
+              wallDetails[0].photos.map((p) => (
+                <img className="thumb" src={p} alt="" />
+              ))
+            }
+          >
+            {wallDetails[0].photos.map((p, i) => {
               return (
-                <div className="carouselImg col-12">
-                  <img src={p} alt="" />
-                </div>
+                <div
+                  className="itemCarouselImg"
+                  style={{ backgroundImage: `url(${p})` }}
+                ></div>
               );
             })}
           </Carousel>
         </div>
-        <div className="col-12 col-md-6"></div>
+        <div className="col-12 col-md-5 offset-md-1 text-justify">
+          <p>{wallDetails[0].text}</p>
+          <p>
+            <strong>Dimentions:</strong> {wallDetails[0].dimensions}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+
+  return (
+    <div className="container wallItem py-5">
+      <button className="btn">
+        <Link to="/walls">
+          <i className="bi bi-backspace"> Back</i>
+        </Link>
+      </button>
+      {wallDetails && wallDisplay}
+      <div className="row text-center">
+        <h4 style={{ color: `rgb(59, 80, 63)` }}>HOW TO RENT OUR WALLS:</h4>
+        <p>
+          Please
+          <Link to="/contact">
+            <strong> SEND US A MESSAGE </strong>
+          </Link>
+          . Also please don't forget to mention the event type, date, venue and
+          time required to be set up by.
+        </p>
+        <p>We will check our availability and get back to you asap.</p>
       </div>
     </div>
   );
