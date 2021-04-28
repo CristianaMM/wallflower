458bc54c3ec6960493deb6935a92d07f643e16d1
Cristiana Monteiro <cristianamelomonteiro@gmail.com>
2021-04-28 15:48:02 +0100

    form corrections

--- src/pages/contactUs/components/FormSubmitMessage.js
+++ src/pages/contactUs/components/FormSubmitMessage.js
@@ -1,15 +1,13 @@
 import React from "react";
 
 export default function Success(props) {
-  const { handleReturn } = props;
+  const { handleReturn, icon, smallMessage, bigMessage, color } = props;
   return (
     <div className="row">
-      <h4 style={{ color: "rgb(59, 80, 63)" }}>
-        <i className="bi bi-check-circle"></i> Thank you!
+      <h4 style={{ color: color }}>
+        <i className={icon}></i> {bigMessage}
       </h4>
-      <p style={{ color: "rgb(59, 80, 63)" }}>
-        Your message has been successfully sent. We will contact you very soon!
-      </p>
+      <p style={{ color: color }}>{smallMessage}</p>
       <div className="col-12 text-center">
         <button className="btn" onClick={handleReturn}>
           return
